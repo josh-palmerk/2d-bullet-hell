9f42f8b2de96dda60e70658c5598fed1e9a4de7a
Josh Palmer <josh_palmerk@byui.edu>
2021-12-01 15:22:30 -0700

    made dodge roll adjustable through game balance

--- bullet-hell/game/attacks/dodge_roll.py
+++ bullet-hell/game/attacks/dodge_roll.py
@@ -1,27 +1,29 @@
 from game.attacks.motion import Motion
 from game import constants
+from game import game_balance as gb
 
 class DodgeRoll(Motion):
     def __init__(self, player):
         super().__init__()
-        self._timer = 70
+        self._roll_length = gb.DODGE_ROLL_DODGE_TIME + gb.DODGE_ROLL_COOLDOWN_TIME
+        self._timer = -1
         self._player = player
 
     def update(self):
         """ update the dodge roll """
-        if self._timer == 70: # start dodge roll
+        if self._timer == self._roll_length: # start dodge roll
             self._player.set_color(constants.DEFAULT_ACTOR_COLOR)
             self._player.set_invincibility(True)
             self._player.set_is_dodging(True)
             self._player.set_is_controllable(False)
+            self._player.set_velocity(self._player.get_velocity().scale(gb.DODGE_ROLL_SPEED_FACTOR))
 
-        elif self._timer == 30: # dodge "hits ground" hence end invinc and half velocity
+
+        elif self._timer == self._roll_length - gb.DODGE_ROLL_DODGE_TIME: # dodge "hits ground" hence end invinc and half velocity
             self._player.set_invincibility(False)
             self._player.set_is_dodging(False)
             self._player.set_color(constants.COLOR_PURPLE)
-            self._player.set_velocity(self._player.get_velocity().scale(0.5))
-            #TODO scale down player velocity
-
+            self._player.set_velocity(self._player.get_velocity().scale(gb.DODGE_ROLL_SLOW_FACTOR))
 
         if self._timer > 0: # tick down the timer to 0
             self.decrement_timer()
@@ -32,7 +34,6 @@ class DodgeRoll(Motion):
             self._player.set_is_motioning_1(False)
             self.decrement_timer()
      
-        
-        else: # reset on new roll
-            self.set_timer(70)
 
+        else: # reset on new roll
+            self.set_timer(self._roll_length)
